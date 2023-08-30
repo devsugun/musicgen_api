5e55204ca9652977427b85165db5f6e970e0ce28
newtecnic <98636891+devsugun@users.noreply.github.com>
2023-08-30 22:44:58 +0100

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,22 +1,16 @@
-from flask import Flask, request, jsonify
+import requests
 
-app = Flask(__name__)
+# Replace with your API endpoint
+API_URL = 'http://127.0.0.1:5000/generate_music'
 
-@app.route('/generate_music', methods=['POST'])
-def generate_music():
-    try:
-        data = request.json
+emotion = 'joyful'  # Replace with the actual user's emotion
 
-        # Extract emotion from the request data
-        emotion = data.get('emotion')
+data = {'emotion': emotion}
+response = requests.post(API_URL, json=data)
 
-        # Replace this with your MusicGen integration logic
-        generated_music = {'emotion': emotion, 'music': 'Generated music data'}
-
-        return jsonify(generated_music), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if response.status_code == 200:
+    generated_music = response.json()
+    # Process the generated_music data as needed
+    print(generated_music)
+else:
+    print(f"Error: {response.status_code}")
